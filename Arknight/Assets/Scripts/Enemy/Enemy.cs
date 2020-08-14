9d3def83ffad024d6ecea157ad2f2b403b1b214d
DESKTOP-6042L56\82105 <yhsk1218@naver.com>
2020-08-14 11:03:52 +0900

    타일맵 관리 추가,  Enemy 주석 추가 및 수정 (임시)

--- Arknight/Assets/Scripts/Enemy/Enemy.cs
+++ Arknight/Assets/Scripts/Enemy/Enemy.cs
@@ -6,15 +6,23 @@ public class Enemy : EnemyManager
 
     /* 생성할 적 클래스 */
 {
+
     // Start is called before the first frame update
-    void Start()
+    new void Start()
     {
-        m_State = STATE.MOVE;
+        base.Start();
+        //m_StartPos = GameObject.Find("Plane").transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    new void Update()
     {
-        
+        base.Update();
+        //if (m_State == STATE.IDLE)
+        //{
+        //    m_Dest = GameObject.Find("Plane (89)").transform.position;
+        //    m_Navi.SetDestination(m_Dest);
+        //    ChangeState(STATE.MOVE);
+        //}
     }
 }
--- Arknight/Assets/Scripts/Node/NodeManager.cs
+++ Arknight/Assets/Scripts/Node/NodeManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -15,58 +16,91 @@ public class NodeManager : MonoBehaviour
 
     public TILESTATE[,] m_State;            // 타일 상태 담은 배열
 
-    public Node[] m_NodeList;
+    public Node[] m_NodeArr;
 
-    int MaxTileX = 10;                      // 타일 가로 갯수
-    int MaxTileY = 9;                       // 타일 세로 갯수
+    int MaxTileX = 10;                      // 타일 총 가로 갯수
+    int MaxTileY = 9;                       // 타일 총 세로 갯수
 
     void Start()
     {
+        m_NodeArr = gameObject.GetComponentsInChildren<Node>();                        // 배열에 노드 싹다 담아줌
+
         // 타일 배열로 저장
         m_State = new TILESTATE[MaxTileY, MaxTileX];
 
-        // 우선 NONE으로 초기화
+        // NONE으로 초기화, 각 노드에 번호 부여
         for (int i = 0; i < MaxTileY; ++i)
         {
-            for(int j = 0; j < MaxTileX; ++j)
+            for (int j = 0; j < MaxTileX; ++j)
             {
                 m_State[i, j] = TILESTATE.NONE;
-                Debug.Log(m_State[i, j]);
-                //Debug.Log(m_State.Length); // 80;
+                m_NodeArr[(i * 10) + j].GetComponent<Node>().SetTileNumber(j, i);
             }
         }
-
-        m_NodeList = gameObject.GetComponentsInChildren<Node>();
-
-        for(int i = 0; i < MaxTileX * MaxTileY; ++i)
-        {
-            m_NodeList[i].m_TileNumber = i;
-        }
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 마우스 좌클릭 시
         if(Input.GetMouseButtonDown(0))
         {
+            // 카메라에서 ray 발사
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // 레이에 맞은 것들
             RaycastHit hit;
 
+            // 레이에 맞은 것 싹다 조사
             if(Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
+                // 맞은 오브젝트의 레이어가 "Ground"라면 (노드라면)
                 if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
                 {
-                    GameObject obj = Instantiate(Resources.Load("Obstacle")) as GameObject;
+                    // 해당 노드의 타일 번호 가져옴
+                    int tileX = hit.transform.GetComponent<Node>().m_TileX;
+                    int tileY = hit.transform.GetComponent<Node>().m_TileY;
 
-                    Vector3 pos = hit.transform.position;
-                    pos.y = 2.5f;
-                    obj.transform.position = pos;
+                    // 가져온 번호의 타일 상태가 NONE이라면
+                    if (m_State[tileY, tileX] == TILESTATE.NONE)
+                    {
+                        // 타워 생성
+                        GameObject obj = Instantiate(Resources.Load("SwordChan")) as GameObject;
+                        obj.GetComponent<Tower>().SetTileNumber(tileX, tileY);
+
+                        // 해당 타일에 배치
+                        Vector3 pos = hit.transform.position;
+                        //pos.y = 2.5f;
+                        obj.transform.position = pos;
+
+                        // 해당 타일을 TOWER상태로 변경
+                        m_State[tileY, tileX] = TILESTATE.TOWER;
+                    }
+                    // 그외면 리턴
+                    else
+                        return;
                 }
                 else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Tower"))
                 {
+                    int tileX = hit.transform.gameObject.GetComponent<Tower>().m_TileX;
+                    int tileY = hit.transform.gameObject.GetComponent<Tower>().m_TileY;
                     Destroy(hit.transform.gameObject);
+                    m_State[tileY, tileX] = TILESTATE.NONE;
                 }
+                
+                //if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                //{
+                //    GameObject obj = Instantiate(Resources.Load("Obstacle")) as GameObject;
+                //    Vector3 pos = hit.transform.position;
+                //    pos.y = 2.5f;
+                //    obj.transform.position = pos;
+                //    m_State[hitTileY, hitTileX] = TILESTATE.TOWER;
+                //
+                //}
+                //else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Tower"))
+                //{
+                //    Destroy(hit.transform.gameObject);
+                //}
             }
         }
     }
