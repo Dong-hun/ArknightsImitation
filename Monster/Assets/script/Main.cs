39938abd48cd0a1e513db714e8b91df0c999b85d
Arutoria-1 <68930821+Arutoria-1@users.noreply.github.com>
2020-08-21 09:37:51 +0900

    Revert "커밋 되돌리기 후 수정"
    This reverts commit 04a9c1ffda8fcb24a968635e3d0e9956d490ead4.

---
+++ Monster/Assets/script/Main.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{//유니티 헤즈페스.등 명령어
+ //일단 이 스킨으로 만들어 보기.
+ //몬스터 이동: 시작하면 자동으로 경로로 이동.
+ // Start is called before the first frame update
+  public  GameObject obj;
+
+    Main m_Main;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MakingD();
+
+
+    }
+    void MakingD()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+                obj   = Instantiate(Resources.Load("Cube")) as GameObject;
+        }
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+
+            Destroy(obj.gameObject);
+        }
+    }
+    
+}
