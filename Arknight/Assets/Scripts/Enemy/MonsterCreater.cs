bab447acb74d9643ba54581ae5981ec147d5d294
potentialk0 <64459852+potentialk0@users.noreply.github.com>
2020-08-26 09:40:52 +0900

    몬스터작동중

--- Arknight/Assets/Scripts/Enemy/MonsterCreater.cs
+++ Arknight/Assets/Scripts/Enemy/MonsterCreater.cs
@@ -7,13 +7,16 @@ public class MonsterCreater : MonoBehaviour
     public bool GameOver = false;
     public int MaxMonster = 3;
     public int TotalMonster = 0;
-    public GameObject obj;
+     GameObject obj;
+    public List<Enemy> m_EnemyList;
+    int MonsterCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Monster());
         StartCoroutine(Boss());
+        StartCoroutine(Monster2());
     }
 
     // Update is called once per frame
@@ -36,6 +39,8 @@ public class MonsterCreater : MonoBehaviour
                 GameObject obj = Instantiate(Resources.Load("Enemy/Temp Monster(Moving)")) as GameObject;
                 MonsterCount++;
                 TotalMonster++;
+                m_EnemyList.Add(obj.GetComponent<Enemy>());
+
             }
             else
             {
@@ -44,6 +49,32 @@ public class MonsterCreater : MonoBehaviour
             }
         }
     }
+    IEnumerator Monster2()
+    {//while 문 밖에다가 설정할 것.
+
+        while (!GameOver)
+        {
+
+            if (MonsterCount < MaxMonster)
+            {
+                yield return new WaitForSeconds(3.0f);
+
+                GameObject obj = Instantiate(Resources.Load("Enemy/TURTLES")) as GameObject;
+                MonsterCount++;
+                TotalMonster++;
+                m_EnemyList.Add(obj.GetComponent<Enemy>());
+
+            }
+            else
+            {
+                yield return null;
+
+            }
+
+
+        }
+    }
+
     IEnumerator Boss()
     {
         while (!GameOver)
@@ -53,6 +84,8 @@ public class MonsterCreater : MonoBehaviour
                 yield return new WaitForSeconds(3.0f);
 
                 GameObject obj = Instantiate(Resources.Load("Enemy/3DBOSS")) as GameObject;
+                m_EnemyList.Add(obj.GetComponent<Enemy>());
+
             }
             else
             {
